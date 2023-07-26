9ad8e0b2fde8a4e74bb88ab9e8b3a409d8c6ab6e
Mermaido <106395332+ishitaraina1807@users.noreply.github.com>
2023-07-26 19:10:52 +0530

    Update Introduction.js

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export default class Introduction extends Component {
   render() {
@@ -8,8 +9,8 @@ export default class Introduction extends Component {
        <h1>Level Up Your Web Development Skills with Fun Quizzes!</h1>
        <h2>Welcome to our engaging quizzes for web development! Whether you are a beginner taking your first steps into the world of coding or an experienced developer looking to brush up your skills, our quizzes are tailored to challenge and educate you on various web development concepts.</h2>
        <div className="mt-10 flex items-center justify-right gap-x-6">
-          <a href="#" className="rounded-md bg-custom-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:text-custom-green focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+          <Link to="/quizzes" className="rounded-md bg-custom-black px-3.5 py-2.5 text-lg font-semibold text-white shadow-sm hover:text-custom-green focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</Link>
+          <Link to="/features" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></Link>
         </div>
        </div>
       </div>
