ec8fb77261d6da218406dba4a21c7c2aa48f4ee6
Mermaido <106395332+ishitaraina1807@users.noreply.github.com>
2023-07-29 16:47:40 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,37 @@
 import React, { Component } from 'react'
-import testwebd from './testwebd.png'
+import testwebd1 from './testwebd1.png'
 import { Link } from 'react-router-dom';
 
 export default class NavBar extends Component {
   render() {
 
     return (
-      <div>
-<nav className="bg-custom-black">  
+      <div className=''>
+<nav className="bg-custom-black ">  
   <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div className="relative flex h-16 items-center justify-between">
 
       <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
         <div className="flex flex-shrink-0 items-center">
-          <img className="h-8 w-auto" src={testwebd} alt="Your Company"/>
+          <img className="h-8 w-auto" src={testwebd1} alt="Your Company"/>
         </div>
         <div className="hidden sm:ml-6 sm:block">
           <div className="flex space-x-4">
           <Link
                     to="/"
-                    className="text-white hover:bg-custom-green hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current
+                    className="text-white hover:bg-custom-red hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current
                   >
                     Home
                   </Link>
                   <Link
                     to="/quizzes"
-                    className="text-white hover:bg-custom-green hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    className="text-white hover:bg-custom-red hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                   >
                     Quizzes
                   </Link>
                   <Link
                     to="/features"
-                    className="text-white hover:bg-custom-green hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    className="text-white hover:bg-custom-red hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                   >
                     Features
                   </Link>
@@ -46,19 +46,19 @@ export default class NavBar extends Component {
     <div className="space-y-1 px-2 pb-3 pt-2">
     <Link
                     to="/"
-                    className="text-white hover:bg-custom-green hover:text-white block rounded-md px-3 py-2 text-base font-medium" aria-current
+                    className="text-white hover:bg-custom-red hover:text-white block rounded-md px-3 py-2 text-base font-medium" aria-current
                   >
                     Home
                   </Link>
                   <Link
                     to="/quizzes"
-                    className="text-white hover:bg-custom-green hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                    className="text-white hover:bg-custom-red hover:text-white block rounded-md px-3 py-2 text-base font-medium"
                   >
                     Quizzes
                   </Link>
                   <Link
                     to="/features"
-                    className="text-white hover:bg-custom-green hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                    className="text-white hover:bg-custom-red hover:text-white block rounded-md px-3 py-2 text-base font-medium"
                   >
                     Features
                   </Link>
