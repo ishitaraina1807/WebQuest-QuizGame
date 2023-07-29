d86780f0ac35a836fb5033638093da1f4b213354
Mermaido <106395332+ishitaraina1807@users.noreply.github.com>
2023-07-29 16:43:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import Quizzes from './components/Quizzes';
 import NavBar from './components/NavBar';
 import Introduction from './components/Introduction';
 import HtmlQuiz from './components/HtmlQuiz';
+import CssQuiz from './components/CssQuiz';
+import JsQuiz from './components/JsQuiz';
 import quizData from './htmlsource.json';
+import cssquizData from './csssource.json';
+import jsquizData from './jssource.json';
+import ResultComponent from './components/ResultComponent';
 
 function App() {
   return (
@@ -16,6 +21,9 @@ function App() {
           <Route path="/" element={<Introduction />} />
           <Route path="/quizzes" element={<Quizzes />} />
           <Route path="/html-quiz" element={<HtmlQuiz quizData={quizData} />} />
+          <Route path="/css-quiz" element={<CssQuiz quizData={cssquizData} />} />
+          <Route path="/javascript-quiz" element={<JsQuiz quizData={jsquizData} />} />
+          <Route path="/result" element={<ResultComponent/>} />
         </Routes>
       </div>
     </div>
