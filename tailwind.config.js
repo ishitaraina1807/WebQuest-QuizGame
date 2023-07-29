96638d8f12d31617efd93a68a3da77831bd3f73a
Mermaido <106395332+ishitaraina1807@users.noreply.github.com>
2023-07-29 16:42:53 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'custom-pink': '#Faa9ba', 
-        'custom-black': '#020202', 
+        'custom-magenta': '#55204a', 
+        'custom-black': '#292a29', 
         'custom-green': '#969e42', 
-        'custom-darkyellow': '#deb657', 
-        'custom-blue': '#c0e4ff', 
-        'custom-purple': '#d4c0ff'
+        'custom-darkyellow': '#f5a560', 
+        'custom-grey': '#989189', 
+        'custom-tint': '#fff7f0', 
+        'custom-red': '#ea7564', 
+        'custom-purple': '#755cde'
       },
     },
   },
