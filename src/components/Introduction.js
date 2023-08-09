da15d3fc07cf48a5f37603a12ccb4f09bb813105
Mermaido <ishitaraina18@gmail.com>
2023-08-09 19:10:07 +0530

    Update Introduction.js

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -9,7 +9,7 @@ export default class Introduction extends Component {
        <h1>Level Up Your Web Development Skills with Fun Quizzes!</h1>
        <h2>Welcome to our engaging quizzes for web development! Whether you are a beginner taking your first steps into the world of coding or an experienced developer looking to brush up your skills, our quizzes are tailored to challenge and educate you on various web development concepts.</h2>
        <div className="mt-10 flex items-center justify-right gap-x-6">
-          <Link to="/quizzes" className="rounded-md bg-custom-black px-3.5 py-2.5 text-lg font-semibold text-white shadow-lg hover:bg-custom-red focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</Link>
+          <Link to="/login" className="rounded-md bg-custom-black px-3.5 py-2.5 text-lg font-semibold text-white shadow-lg hover:bg-custom-red focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">LOGIN</Link>
           <Link to="/features" className="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></Link>
         </div>
        </div>
