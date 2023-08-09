ed2a77d261321f7e45df893327c054686d62b6ca
Mermaido <ishitaraina18@gmail.com>
2023-08-09 19:11:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,19 +10,28 @@ import JsQuiz from './components/JsQuiz';
 import quizData from './htmlsource.json';
 import cssquizData from './csssource.json';
 import jsquizData from './jssource.json';
+import Login from './components/Login';
+import AuthProvider from './AuthProvider'; 
+import SignUp from './components/SignUp';
+import Features from './components/Features';
 
 function App() {
   return (
     <div>
       <NavBar />
       <div className='app'>
-        <Routes>
-          <Route path="/" element={<Introduction />} />
-          <Route path="/quizzes" element={<Quizzes />} />
-          <Route path="/html-quiz" element={<HtmlQuiz quizData={quizData} />} />
-          <Route path="/css-quiz" element={<CssQuiz quizData={cssquizData} />} />
-          <Route path="/javascript-quiz" element={<JsQuiz quizData={jsquizData} />} />
-        </Routes>
+        <AuthProvider> 
+          <Routes>
+            <Route path="/" element={<Introduction />} />
+            <Route path="/features" element={<Features />} />
+            <Route path="/quizzes" element={<Quizzes />} />
+            <Route path="/html-quiz" element={<HtmlQuiz quizData={quizData} />} />
+            <Route path="/css-quiz" element={<CssQuiz quizData={cssquizData} />} />
+            <Route path="/javascript-quiz" element={<JsQuiz quizData={jsquizData} />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp/>} />
+          </Routes>
+        </AuthProvider>
       </div>
     </div>
   );
