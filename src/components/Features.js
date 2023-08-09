ba3c9632f6cd2f3f43de35bebfe7256a7d45b5b8
Mermaido <ishitaraina18@gmail.com>
2023-08-09 19:09:47 +0530

    Update Features.js

--- src/components/Features.js
+++ src/components/Features.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Features extends Component {
   render() {
     return (
-      <div>
-        
+      <div className='text-custom-black p-16 features'>
+        Welcome to TestWebD, the ultimate platform to test your knowledge on HTML, CSS, and JavaScript through engaging quizzes. Our site offers an interactive learning experience that challenges your understanding of web development concepts. To access the quizzes, simply log in to your TestWebD account. Don't have an account yet? No worries, you can easily sign up and join the learning journey. Once logged in, you can choose your preferred category and dive into the quiz of your choice. Each quiz consists of 10 thought-provoking questions, each with four answer choices, only one of which is correct. Make sure to consider your options carefully, as answers are locked in once selected. After completing the quiz, you'll receive immediate feedback on your score and the time it took to complete. TestWebD is your gateway to enhancing your web development knowledge in an engaging and fun way. Start your journey today and challenge yourself to become a web development pro!
       </div>
     )
   }
