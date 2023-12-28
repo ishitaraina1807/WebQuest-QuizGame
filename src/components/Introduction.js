8b307901835c09873d8da8db81769f264326531c
ishitaraina1807 <ishitaraina18@gmail.com>
2023-12-28 22:35:27 +0530

    main page UI

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -6,7 +6,7 @@ export default class Introduction extends Component {
     return (
       <div>
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 container mt-36 transition-all duration-1000">
-          <h1 className=''>Level Up Your Web Development Skills with Fun Quizzes!</h1>
+          <h1 className=''>Level Up Your Web Development Knowledge with Fun Quizzes!</h1>
           <h2>Welcome to our engaging quizzes for web development! Whether you are a beginner taking your first steps into the world of coding or an experienced developer looking to brush up your skills, our quizzes are tailored to challenge and educate you on various web development concepts.</h2>
           <div className="mt-10 flex items-center justify-right gap-x-6">
             <Link to="/login" className="rounded-md bg-white px-5 py-2.5 text-md font-semibold text-[#152039] transform hover:scale-110 transition duration-300">LOGIN</Link>
